2b5ba5115a0b57e80bc5565e7180a40460b1d320
mralinp <alinp@vancosys.com>
2023-01-06 11:13:15 +0330

    init project

---
+++ SQLiteORM/SQLiteORM/SQLiteORM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteORM
+{
+    public class DB
+    {
+        private static DB s_instance;
+
+        private DB(string dbName) { 
+            throw new NotImplementedException();
+        }
+
+        public static void Connect(string dbName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ SQLiteORMTest/DBTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteORMTest
+{
+    [TestFixture]
+    public class DBTest
+    {
+        [Test]
+        [TestCase("testdb.db")]
+        [TestCase("./db/testdb.db")]
+        public void ConnectionTest(string dbName) {
+            SQLiteORM.DB.Connect(dbName);
+        }
+
+        [Test]
+        [TestCase("testdb.db", "CREATE TABLE test(id PRIMARY_KEY, user_name TEXT, password TEXT, created_at DATE_TIME);")]
+        public void ExecuteCMD(string dbName, string cmd) { 
+                    
+        }
+
+    }
+}
